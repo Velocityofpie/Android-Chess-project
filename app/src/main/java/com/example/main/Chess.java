0d2d1603370f2c1f4a5e49e3e9bc161d82d3303b
Joshua <Joshuamuq@gmail.com>
2021-04-29 17:57:36 -0400

    XML fixes

--- app/src/main/java/com/example/main/Chess.java
+++ app/src/main/java/com/example/main/Chess.java
@@ -2,13 +2,29 @@ package com.example.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Chess extends AppCompatActivity {
-
+    private  Button Bpvp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_interface);
+        Bpvp= (Button) findViewById(R.id.Bpvp);
+        Bpvp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGame();
+
+            }
+        });
+
+    }
+    public void openGame(){
+
+
     }
 }
