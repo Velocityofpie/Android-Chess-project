5cf793ed2fbb1f640315f9f41f2ed0cb36e877c3
jvl50 <jvl50@scarletmail.rutgers.edu>
2021-04-29 17:56:47 -0400

    home screen buttons change content view

--- app/src/main/java/com/example/main/Chess.java
+++ app/src/main/java/com/example/main/Chess.java
@@ -3,12 +3,33 @@ package com.example.main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Chess extends AppCompatActivity {
+
+    Button playerVplayer, gamesPlayed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_interface);
+
+        playerVplayer = findViewById(R.id.playerVplayer);
+        gamesPlayed = findViewById(R.id.gamesPlayed);
+
+        playerVplayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.play_chess);
+            }
+        });
+
+        gamesPlayed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.saved_games);
+            }
+        });
     }
 }
