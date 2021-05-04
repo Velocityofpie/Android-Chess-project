60161ccecd6f574d5b2b925d1005f25f0effd7f1
Joshua <Joshuamuq@gmail.com>
2021-05-04 15:30:51 -0400

    fixes

--- app/src/main/java/com/example/main/Chess.java
+++ app/src/main/java/com/example/main/Chess.java
@@ -27,29 +27,29 @@ import pieces.Piece;
 import pieces.Queen;
 import pieces.Rook;
 
-public class Chess extends AppCompatActivity {
+public class Chess<exception_var> extends AppCompatActivity {
 
-    Button playerVplayer, gamesPlayed;
-    ImageButton btn_a1, btn_b1, btn_c1, btn_d1, btn_e1, btn_f1, btn_g1, btn_h1,
-            btn_a2, btn_b2, btn_c2, btn_d2, btn_e2, btn_f2, btn_g2, btn_h2,
-            btn_a3, btn_b3, btn_c3, btn_d3, btn_e3, btn_f3, btn_g3, btn_h3,
-            btn_a4, btn_b4, btn_c4, btn_d4, btn_e4, btn_f4, btn_g4, btn_h4,
-            btn_a5, btn_b5, btn_c5, btn_d5, btn_e5, btn_f5, btn_g5, btn_h5,
-            btn_a6, btn_b6, btn_c6, btn_d6, btn_e6, btn_f6, btn_g6, btn_h6,
-            btn_a7, btn_b7, btn_c7, btn_d7, btn_e7, btn_f7, btn_g7, btn_h7,
-            btn_a8, btn_b8, btn_c8, btn_d8, btn_e8, btn_f8, btn_g8, btn_h8;
+        Button playerVplayer, gamesPlayed, btnDraw;
+        ImageButton btn_a1, btn_b1, btn_c1, btn_d1, btn_e1, btn_f1, btn_g1, btn_h1,
+                btn_a2, btn_b2, btn_c2, btn_d2, btn_e2, btn_f2, btn_g2, btn_h2,
+                btn_a3, btn_b3, btn_c3, btn_d3, btn_e3, btn_f3, btn_g3, btn_h3,
+                btn_a4, btn_b4, btn_c4, btn_d4, btn_e4, btn_f4, btn_g4, btn_h4,
+                btn_a5, btn_b5, btn_c5, btn_d5, btn_e5, btn_f5, btn_g5, btn_h5,
+                btn_a6, btn_b6, btn_c6, btn_d6, btn_e6, btn_f6, btn_g6, btn_h6,
+                btn_a7, btn_b7, btn_c7, btn_d7, btn_e7, btn_f7, btn_g7, btn_h7,
+                btn_a8, btn_b8, btn_c8, btn_d8, btn_e8, btn_f8, btn_g8, btn_h8;
 
-    Button btnAI;
+        Button btnAI;
 
-    TextView txtTurn;
+        TextView txtTurn;
 
-    String input = "";
-    String output = "";
-    String initialPiece;
-    boolean gameStillRunning = true;
+        String input = "";
+        String output = "";
+        String initialPiece;
+        boolean gameStillRunning = true;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_interface);
 
@@ -129,8 +129,6 @@ public class Chess extends AppCompatActivity {
                 btnAI = findViewById(R.id.btnAI);
 
 
-
-
                 txtTurn = findViewById(R.id.txtTurn);
                 txtTurn.setText(R.string.whiteTurn);
 
@@ -157,6 +155,16 @@ public class Chess extends AppCompatActivity {
                     }
                 }
 
+//                btnDraw.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        openDialog();
+//                    }
+//
+//                    public void openDialog() {
+//                    }
+//                });
+
 
                 btn_a1.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -182,6 +190,7 @@ public class Chess extends AppCompatActivity {
                     }
                 });
 
+
                 btn_a2.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -205,6 +214,7 @@ public class Chess extends AppCompatActivity {
                         }
                     }
                 });
+
 
                 btn_a3.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -1731,11 +1741,9 @@ public class Chess extends AppCompatActivity {
                 });
 
 
-
-
-
             }
         });
+
 
         gamesPlayed.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -1751,7 +1759,9 @@ public class Chess extends AppCompatActivity {
 //        if(presscounter==2)
 //            super.onBackPressed();
 //    }
-    private ImageButton getImageButton(ImageButton[][] buttons, String target) {
+
+
+        private ImageButton getImageButton (ImageButton[][]buttons, String target){
 
         for (int i = 0; i < buttons.length; i++) {
             for (int j = 0; j < buttons[0].length; j++) {
@@ -1763,7 +1773,7 @@ public class Chess extends AppCompatActivity {
         return buttons[0][0];
     }
 
-    public void printButtons(ImageButton[][] arr, chess.Board board) {
+        public void printButtons (ImageButton[][]arr, chess.Board board){
 
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
@@ -1807,4 +1817,5 @@ public class Chess extends AppCompatActivity {
             }
         }
     }
+
 }
